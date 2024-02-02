89e34417f95cca138e1e316353b5013be5ea775c
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-02-02 12:18:32 -0600

    Added encode function

--- Cryptography/Caesar Cipher/Encrypt_Caesar_Cipher.py
+++ Cryptography/Caesar Cipher/Encrypt_Caesar_Cipher.py
@@ -8,3 +8,18 @@ this will decode and encrypt a message with a key
 '''
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+def encode():
+  print("\nWe are Encodeing now!!!\n")
+  key = input("What is the Key: ")
+  incoming_message = input("\nWhat is the message I need to Encode?\n")
+  outgoing_message = ""
+
+  for text in incoming_message:
+      if text in alphabet:
+          char = alphabet.find(text)
+          outgoing_message += alphabet[(char - int(key)) % 26]
+      else:
+          outgoing_message += text
+
+  print("\nHere is your coded message.\n" + outgoing_message)
