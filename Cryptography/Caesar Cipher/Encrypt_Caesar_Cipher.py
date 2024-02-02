407e3a4634cd6a731f95b2ba94bf3d3f4222a9ac
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-02-02 12:52:29 -0600

    added a comment in the function

--- Cryptography/Caesar Cipher/Encrypt_Caesar_Cipher.py
+++ Cryptography/Caesar Cipher/Encrypt_Caesar_Cipher.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 '''
 Caesar cipher
 by Wayne Stock
@@ -10,29 +9,27 @@ this will decode and encrypt a message with a key
 
 ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 
-
 def encode():
-  
-  
-  # Key Validation
-  try:
-    key = int(input("What is the Key: "))
-  except ValueError:
-    print("Invalid key. Please enter a valid integer.")
-    return
+    """
+    Encodes a message using the Caesar cipher.
+    """
+    try:
+        key = int(input("What is the Key: "))
+    except ValueError:
+        print("Invalid key. Please enter a valid integer.")
+        return
 
-        
-  incoming_message = input("\nWhat is the message I need to Encode?\n")
-  outgoing_message = ""
+    incoming_message = input("\nWhat is the message I need to Encode?\n")
+    outgoing_message = ""
 
-  for text in incoming_message:
-      if text in ALPHABET:
-          char = ALPHABET.find(text)
-          outgoing_message += ALPHABET[(char - int(key)) % 26]
-      else:
-          outgoing_message += text
+    for text in incoming_message:
+        if text in ALPHABET:
+            char = ALPHABET.find(text)
+            outgoing_message += ALPHABET[(char - key) % 26]
+        else:
+            outgoing_message += text
 
-  print("\nHere is your coded message.\n" + outgoing_message)
+    print("\nHere is your coded message.\n" + outgoing_message)
 
 # Call the function
 encode()
