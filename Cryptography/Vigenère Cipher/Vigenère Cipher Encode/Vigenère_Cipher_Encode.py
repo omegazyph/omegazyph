10daa95f966d990f47eaab1ae7668c62b4efdc83
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-02-03 12:19:55 -0600

    added Vigenere Cipher from my computer

---
+++ Cryptography/Vigenère Cipher/Vigenère Cipher Encode/Vigenère_Cipher_Encode.py
@@ -0,0 +1,32 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+def vigenere_decode(message, keyword):
+  keyword_phrase = ""
+  keyword_index = 0
+
+  for character in message:
+    if keyword_index >= len(keyword):
+      keyword_index = 0
+    if character in alphabet:
+      keyword_phrase += keyword[keyword_index]
+      keyword_index += 1
+    else:
+      keyword_phrase += character
+
+  encoded_message = ""
+
+  for i in range(len(message)):
+    if message[i] in alphabet:
+      old_character_index = alphabet.find(message[i])
+      offset_index = alphabet.find(keyword_phrase[i])
+      new_character = alphabet[(old_character_index - offset_index) % 26]
+      encoded_message += new_character
+    else:
+      encoded_message += message[i]
+    
+  return encoded_message
+
+vigenere_message = input("Enter your Message:\n")
+vigenere_keyword = input("Enter your keyWord:\n")
+
+print("Here is your Encoded message:\n" + vigenere_decode(vigenere_message, vigenere_keyword))
---
+++ Cryptography/Vigenère Cipher/Vigenère_Cipher_Decode/Vigenère_Cipher_Decode.py
@@ -0,0 +1,32 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+def vigenere_decode(message, keyword):
+  keyword_phrase = ""
+  keyword_index = 0
+
+  for character in message:
+    if keyword_index >= len(keyword):
+      keyword_index = 0
+    if character in alphabet:
+      keyword_phrase += keyword[keyword_index]
+      keyword_index += 1
+    else:
+      keyword_phrase += character
+
+  decoded_message = ""
+
+  for i in range(len(message)):
+    if message[i] in alphabet:
+      old_character_index = alphabet.find(message[i])
+      offset_index = alphabet.find(keyword_phrase[i])
+      new_character = alphabet[(old_character_index + offset_index) % 26]
+      decoded_message += new_character
+    else:
+      decoded_message += message[i]
+    
+  return decoded_message
+
+vigenere_message = input("Enter your message to Decode:\n")
+vigenere_keyword = input("Enter the KeyWord:\n")
+
+print("Here is the message:\n" + vigenere_decode(vigenere_message, vigenere_keyword))
