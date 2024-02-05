9838ab6118f84340c3d2f0d46fc1f352aeb80ec9
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-02-05 09:08:10 -0600

    changed aplphabet to a contents

--- Cryptography/Vigenere Cipher/Vigenere_Cipher_Decode/Decode.py
+++ Cryptography/Vigenere Cipher/Vigenere_Cipher_Decode/Decode.py
@@ -6,7 +6,7 @@ created 2024-02-05
 this will decode the vigenere code from one file to another
 '''
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 
 def vigenere_decode(message, keyword):
   keyword_phrase = ""
@@ -15,7 +15,7 @@ def vigenere_decode(message, keyword):
   for character in message:
     if keyword_index >= len(keyword):
       keyword_index = 0
-    if character in alphabet:
+    if character in ALPHABET:
       keyword_phrase += keyword[keyword_index]
       keyword_index += 1
     else:
@@ -24,10 +24,10 @@ def vigenere_decode(message, keyword):
   decoded_message = ""
 
   for i in range(len(message)):
-    if message[i] in alphabet:
-      old_character_index = alphabet.find(message[i])
-      offset_index = alphabet.find(keyword_phrase[i])
-      new_character = alphabet[(old_character_index + offset_index) % 26]
+    if message[i] in ALPHABET:
+      old_character_index = ALPHABET.find(message[i])
+      offset_index = ALPHABET.find(keyword_phrase[i])
+      new_character = ALPHABET[(old_character_index + offset_index) % 26]
       decoded_message += new_character
     else:
       decoded_message += message[i]
