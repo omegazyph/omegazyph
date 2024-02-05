be29bba5519b3eeb60c81677b4be24b0e3bc3238
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-02-05 09:12:27 -0600

    added classes

--- Cryptography/Vigenere Cipher/Vigenere_Cipher_Decode/Decode.py
+++ Cryptography/Vigenere Cipher/Vigenere_Cipher_Decode/Decode.py
@@ -8,33 +8,36 @@ this will decode the vigenere code from one file to another
 
 ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 
-def vigenere_decode(message, keyword):
-  keyword_phrase = ""
-  keyword_index = 0
+class main:
 
-  for character in message:
-    if keyword_index >= len(keyword):
-      keyword_index = 0
-    if character in ALPHABET:
-      keyword_phrase += keyword[keyword_index]
-      keyword_index += 1
-    else:
-      keyword_phrase += character
+  def vigenere_decode(message, keyword):
+    keyword_phrase = ""
+    keyword_index = 0
 
-  decoded_message = ""
+    for character in message:
+      if keyword_index >= len(keyword):
+        keyword_index = 0
+      if character in ALPHABET:
+        keyword_phrase += keyword[keyword_index]
+        keyword_index += 1
+      else:
+        keyword_phrase += character
 
-  for i in range(len(message)):
-    if message[i] in ALPHABET:
-      old_character_index = ALPHABET.find(message[i])
-      offset_index = ALPHABET.find(keyword_phrase[i])
-      new_character = ALPHABET[(old_character_index + offset_index) % 26]
-      decoded_message += new_character
-    else:
-      decoded_message += message[i]
+    decoded_message = ""
+
+    for i in range(len(message)):
+      if message[i] in ALPHABET:
+        old_character_index = ALPHABET.find(message[i])
+        offset_index = ALPHABET.find(keyword_phrase[i])
+        new_character = ALPHABET[(old_character_index + offset_index) % 26]
+        decoded_message += new_character
+      else:
+        decoded_message += message[i]
     
-  return decoded_message
+    return decoded_message
 
-vigenere_message = input("Enter your message to Decode:\n")
-vigenere_keyword = input("Enter the KeyWord:\n")
+class UserInput:
+  vigenere_message = input("Enter your message to Decode:\n")
+  vigenere_keyword = input("Enter the KeyWord:\n")
 
 print("Here is the message:\n" + vigenere_decode(vigenere_message, vigenere_keyword))
