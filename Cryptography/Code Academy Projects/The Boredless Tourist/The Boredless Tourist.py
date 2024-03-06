a0e9d338b1b7e0a8026f6e585f46a25c57d36a89
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-06 16:07:36 -0600

    Added function to generate message for traveler and present attractions they might be interested in.

--- Cryptography/Code Academy Projects/The Boredless Tourist/The Boredless Tourist.py
+++ Cryptography/Code Academy Projects/The Boredless Tourist/The Boredless Tourist.py
@@ -58,5 +58,28 @@ def find_attractions(destination,interests):
         attractions_with_interest.append(possible_attraction[0])
   return attractions_with_interest      
 
-la_arts = find_attractions("Los Angeles, USA", ['art'])
-print(la_arts)
+#la_arts = find_attractions("Los Angeles, USA", ['art'])
+#print(la_arts)
+
+
+# See The Parts of a city You want to see
+def get_attractions_for_traveler(traveler):
+  traveler_destination = traveler[1]
+  traveler_interests = traveler[2]
+  traveler_attractions = find_attractions(traveler_destination, traveler_interests)
+
+  interests_string = "Hi  " + traveler[0] + ", we think you/'ll like these places around " +  traveler_destination + ":"
+  
+  for i in range(len(traveler_attractions)):
+    if traveler_attractions[-1] == traveler_attractions[i]:
+      interests_string += "the " + traveler_attractions[i] + "."
+    else:
+      interests_string += "the "+ traveler_attractions[i] + ","
+  return interests_string
+
+
+smills_france = get_attractions_for_traveler(['Dereck Smill', 'Paris, France', ['monument']])
+
+print(smills_france)
+
+  
