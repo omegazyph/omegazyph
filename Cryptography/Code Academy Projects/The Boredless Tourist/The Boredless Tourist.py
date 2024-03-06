78a3a5026f4cfd6ed7d7508542752ef587aee28d
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-06 10:15:48 -0600

    Added logic to find traveler destinations and convert to internal data

--- Cryptography/Code Academy Projects/The Boredless Tourist/The Boredless Tourist.py
+++ Cryptography/Code Academy Projects/The Boredless Tourist/The Boredless Tourist.py
@@ -1,13 +1,18 @@
-# Define a list of destinations
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
 
-# Define a test traveler with name, destination, and list of interests
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
-
-
-# Define a function to get the index of a destination in the destinations list
 def get_destination_index(destination):
-    destination_index = destinations.index(destination)
-    return destination_index
-    
+  destination_index = destinations.index(destination)
+  return destination_index
+
+
+def get_traveler_location(traveler):
+  traveler_destination = traveler[1]
+  traveler_destination_index = get_destination_index(traveler_destination)
+  return traveler_destination_index
+
+test_destination_index = get_traveler_location(test_traveler)
+print(test_destination_index)
+
+  
