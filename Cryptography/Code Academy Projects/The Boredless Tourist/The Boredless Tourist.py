5328933c854e9d7b619e577e75d0c56fce541841
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-05 21:01:28 -0600

    Added two lists destinations and test_traveler

---
+++ Cryptography/Code Academy Projects/The Boredless Tourist/The Boredless Tourist.py
@@ -0,0 +1,5 @@
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cariro, Egypt"]
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+
