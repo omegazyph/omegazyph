9303ac7d71b711f4842010274cd2b8c3e178ae1c
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-06 12:17:19 -0600

    Created attractions and functionality for adding new attractions

--- Cryptography/Code Academy Projects/The Boredless Tourist/The Boredless Tourist.py
+++ Cryptography/Code Academy Projects/The Boredless Tourist/The Boredless Tourist.py
@@ -1,4 +1,4 @@
-destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
@@ -13,6 +13,28 @@ def get_traveler_location(traveler):
   return traveler_destination_index
 
 test_destination_index = get_traveler_location(test_traveler)
-print(test_destination_index)
+#print(test_destination_index)
 
-  
+
+attractions = [[] for destination in destinations]
+#print(attractions)
+
+
+def add_attraction(destination,attraction):
+  destination_index = get_destination_index(destination)
+  attraction_for_destination = attractions[destination_index].append(attraction)
+  return
+
+add_attraction("Los Angeles, USA",["Venice Beach",['beach']])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+print(attractions)
