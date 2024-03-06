cb65aadde8c7c45a2f501bb54e0a8becb4cd7c2d
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-06 08:39:06 -0600

    finsied up the function

--- Cryptography/Code Academy Projects/The Boredless Tourist/The Boredless Tourist.py
+++ Cryptography/Code Academy Projects/The Boredless Tourist/The Boredless Tourist.py
@@ -4,4 +4,7 @@ test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
 
 def get_distination_index(destination):
-    
+  destination_index = destinations.index(destination)
+  return destination_index
+
+print(get_distination_index("Los Angeles, USA"))    
