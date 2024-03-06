ea1cc5d58fd7c276cf23be18d1ccaa5a6b0339ab
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-06 15:03:44 -0600

    Added interest finder logic

--- Cryptography/Code Academy Projects/The Boredless Tourist/The Boredless Tourist.py
+++ Cryptography/Code Academy Projects/The Boredless Tourist/The Boredless Tourist.py
@@ -1,7 +1,11 @@
+
+# Setting up Project
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
+
+# Travelling to Farway Lands
 def get_destination_index(destination):
   destination_index = destinations.index(destination)
   return destination_index
@@ -16,6 +20,7 @@ test_destination_index = get_traveler_location(test_traveler)
 #print(test_destination_index)
 
 
+# Visiting Interesting Places
 attractions = [[] for destination in destinations]
 #print(attractions)
 
@@ -37,4 +42,21 @@ add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]]
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
 
-print(attractions)
+#print(attractions)
+
+# Finding the Best Places to go
+def find_attractions(destination,interests):
+  destination_index = get_destination_index(destination)
+  attractions_in_city = attractions[destination_index]
+  attractions_with_interest = []
+  for attraction in attractions_in_city:
+    possible_attraction = attraction
+    attraction_tags = attraction[1]
+    
+    for interest in interests:
+      if interest in attraction_tags:
+        attractions_with_interest.append(possible_attraction[0])
+  return attractions_with_interest      
+
+la_arts = find_attractions("Los Angeles, USA", ['art'])
+print(la_arts)
