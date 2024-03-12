831129109c6c6a68e15680a4b66b7bd7afec9731
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-12 15:59:27 -0500

    added __repr__ to shoppers and employee classes

--- Code Academy Projects/Game/Store/Store.py
+++ Code Academy Projects/Game/Store/Store.py
@@ -17,27 +17,33 @@ class Employee:
     self.department = department
 
   def working(self):
-    # Calls its method .getName()
-    print("{name} is {age} working in {department}".format(name = self.getName(),age = self.age , department= self.department))
-
-  def getName(self):
-    # Accesses the name variable
-    return self.name
-''' Employee info
-
-
+    print("{name} is {age} working in {department}".format(name = self.name,age = self.age , department= self.department))
+  
+  def __repr__(self):
+    return "I have {name} woking in {department} that is {age} years old".format(name = self.name, department = self.department, age = self.age)
+'''#Employee info
 bob = Employee("Bob", 36, "perishables")
+print(repr(bob))
 bob.working()
 
+
 jane = Employee("Jane", 18, "Deli")
+print(repr(jane))
 jane.working()
 '''
 
+
 class Shopper:
-  def __init__(self,input_name, input_age):
+  def __init__(self,input_name, input_age, input_ID):
     self.name = input_name
     self.age = input_age
-    self.id_check = False
+    self.id = input_ID
+
+  def id_check(self):
+    if self.id == True:
+      self.check_age()  
+    else:
+       print("{name} is NOT old enough to buy cigerttes".format(name = self.name))
 
   def check_age(self):
     minimum_age = 21
@@ -45,11 +51,15 @@ class Shopper:
     if self.age >= minimum_age:
       print("{name} is old enough to buy cigerttes".format(name = self.name))
     else:
-      print("{name} is NOT old enough to buy cigerttes".format(name = self.name))
+     print("somthing is wrong")
 
+  def __repr__(self):
+    return " The customer {name} is {age} and has id {id}.".format(name = self.name, age = self.age, id = self.id)
 #shopper info
-richard = Shopper("Richard", 21)
-richard.check_age()
+richard = Shopper("Richard", 21, True)
+print(repr(richard))
+#richard.id_check()
 
-mary = Shopper("Mary", 12)
-mary.check_age()
+mary = Shopper("Mary", 12, False)
+print(repr(mary))
+#mary.id_check()
