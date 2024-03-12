4ab45a05609a9f7ed40d027a4ba76566ee8a243a
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-12 14:56:20 -0500

    added shopper class

--- Code Academy Projects/Game/Store/Store.py
+++ Code Academy Projects/Game/Store/Store.py
@@ -9,6 +9,7 @@ checkout class
 over head class
 '''
 
+
 class Employee:
   def __init__(self, name, age, department):
     self.name = name
@@ -22,11 +23,35 @@ class Employee:
   def getName(self):
     # Accesses the name variable
     return self.name
-'''
+''' Employee info
+
 
 bob = Employee("Bob", 36, "perishables")
 bob.working()
 
 jane = Employee("Jane", 18, "Deli")
 jane.working()
+'''
+
+class Shopper:
+  def __init__(self,input_name, input_age):
+    self.name = input_name
+    self.age = input_age
+
+  def check_age(self):
+    minimum_age = 21
+
+    if self.age >= minimum_age:
+      print("{name} is old enough to buy cigerttes".format(name = self.getName()))
+    else:
+      print("{name} is NOT old enough to buy cigerttes".format(name = self.getName()))
+
+  def getName(self):
+    return self.name
+'''shopper info
+richard = Shopper("Richard", 21)
+richard.check_age()
+
+mary = Shopper("Mary", 12)
+mary.check_age()
 '''
