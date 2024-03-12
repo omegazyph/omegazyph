8e43aeeb75b53c0c454ec64ab34ec1c6c7aaa69d
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-12 15:22:25 -0500

    remove a fucntion git name

--- Code Academy Projects/Game/Store/Store.py
+++ Code Academy Projects/Game/Store/Store.py
@@ -37,21 +37,19 @@ class Shopper:
   def __init__(self,input_name, input_age):
     self.name = input_name
     self.age = input_age
+    self.id_check = False
 
   def check_age(self):
     minimum_age = 21
 
     if self.age >= minimum_age:
-      print("{name} is old enough to buy cigerttes".format(name = self.getName()))
+      print("{name} is old enough to buy cigerttes".format(name = self.name))
     else:
-      print("{name} is NOT old enough to buy cigerttes".format(name = self.getName()))
+      print("{name} is NOT old enough to buy cigerttes".format(name = self.name))
 
-  def getName(self):
-    return self.name
-'''shopper info
+#shopper info
 richard = Shopper("Richard", 21)
 richard.check_age()
 
 mary = Shopper("Mary", 12)
 mary.check_age()
-'''
