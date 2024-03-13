9106a6a69d47db931577ab1b3bfc2d4d81eb4d72
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-12 21:34:57 -0500

    added betting function

--- Portfolio Projects/Black_Jack/Black Jack.py
+++ Portfolio Projects/Black_Jack/Black Jack.py
@@ -18,9 +18,20 @@ class Player:
             add = int(input("{name} How much would you like to bring to the table:\n>> ".format(name = self.name)))
             self.account.append(add)
         else:
-            print("{name} Good Luck!".format(name = self.name))
+            print("{name} you need money to play".format(name = self.name))
 
-    
+    def betting(self):
+        money = int(input("{name} How much would you like to Bet:\n>> ".format(name = self.name)))
+        game = int(input("choose a number 1 or 2:\n>> "))
+        if game == 1: # will add money to the account
+            print("YOU WIN")
+            self.account = [num + money for num in self.account]
+            
+        else:
+            print("Sorry you lose")
+            self.account = [num - money for num in self.account]
+
+
 
     def __repr__(self):
         return "{name} has {money} left on the table.".format(name = self.name, money = self.account)
@@ -28,8 +39,11 @@ class Player:
 #player info
 new_player = Player("Player_1", 0)
 new_player.add_money()
+new_player.betting()
 print(repr(new_player))
 
+'''
 player2 = Player("player_2", 0)
 player2.add_money()
 print(repr(player2))
+'''
