67760183117e2084714ceb8dbb5fbe64f36c787a
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-13 10:20:46 -0500

    add content for Card_class

---
+++ Portfolio Projects/Black_Jack/Classes/Deck_class.py
@@ -0,0 +1,21 @@
+import random
+from Card_class import *
+
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.create_deck()
+
+    def create_deck(self):
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+        for suit in suits:
+            for value in values:
+                self.cards.append(Card(suit, value))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def deal_card(self):
+        return self.cards.pop()
