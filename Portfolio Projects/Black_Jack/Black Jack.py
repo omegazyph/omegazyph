4b0c02ba0e041b71016c69f8d8c6153172b2ce70
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-12 19:19:32 -0500

    added a header in the file blackJack.py

---
+++ Portfolio Projects/Black_Jack/Black Jack.py
@@ -0,0 +1,6 @@
+###################################
+# BlackJack
+# By Wayne Stock
+# Started 2024-03-12
+# It is the game of 21 playing aginst the dealer
+################################################
