3cb1d0776d9b21efb461b42551f92feb65842a6e
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-12 19:37:44 -0500

    added functions to player and tested them out

--- Portfolio Projects/Black_Jack/Black Jack.py
+++ Portfolio Projects/Black_Jack/Black Jack.py
@@ -4,3 +4,22 @@
 # Started 2024-03-12
 # It is the game of 21 playing aginst the dealer
 ################################################
+
+
+class Player:
+    def __init__(self, input_name, input_bet):
+        self.name = input_name
+        self.bet = input_bet
+        self.account = []
+
+    def add_money(self):
+        add = int(input("How much would you like to bring to the table:\n>> "))
+        self.account.append(add)
+
+    def __repr__(self):
+        return "{name} has {money} left on the table.".format(name = self.name, money = self.account)
+
+#player info
+new_player = Player("Player_1", 0)
+new_player.add_money()
+print(repr(new_player))
