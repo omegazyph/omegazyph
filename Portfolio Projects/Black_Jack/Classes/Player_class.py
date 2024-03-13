ad4f6a4203ffbb2ec0c2f052699a3cc8f92693f6
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-13 10:48:42 -0500

    added content to the hand class

--- Portfolio Projects/Black_Jack/Classes/Player_class.py
+++ Portfolio Projects/Black_Jack/Classes/Player_class.py
@@ -1,58 +1,22 @@
-###################################
-# BlackJack Player Class
-# By Wayne Stock
-# Started 2024-03-12
-# It is the game of 21 playing aginst the dealer
-################################################
-
-import random
-
+from Classes.Hand_class import *
 
 class Player:
-    def __init__(self, input_name, input_bet):
-        self.name = input_name
-        self.bet = input_bet
-        self.account = 0
+    def __init__(self, name):
+        self.name = name
+        self.hand = Hand()
 
-    def add_money(self):
-        choice = input("{name} would you like to add more money to the table:\n>> ".format(name = self.name).lower())
-        if choice == "yes":
-            add = int(input("{name} How much would you like to bring to the table:\n>> ".format(name = self.name)))
-            add += self.account
-        else:
-            print("{name} you need money to play".format(name = self.name))
+    def add_card_to_hand(self, card):
+        self.hand.add_card(card)
 
-    def betting(self):
-        money = int(input("{name} How much would you like to Bet:\n>> ".format(name = self.name)))
-    
+    def get_hand_value(self):
+        return self.hand.calculate_value()
 
-        game = int(input("choose a number 1 or 2:\n>> "))
-        if game == 1: # will add money to the account
-            print("YOU WIN")
-            add = money + money *2
-            self.account += add
-            
-        elif game == 2:
-            print("Sorry you lose")
-            self.account -= money
-            
+class Dealer:
+    def __init__(self):
+        self.hand = Hand()
 
+    def add_card_to_hand(self, card):
+        self.hand.add_card(card)
 
-
-    def __repr__(self):
-        return "{name} has {money} left on the table.".format(name = self.name, money = self.account)
-
-
-
-
-#player info
-new_player = Player("Player_1", 0)
-new_player.add_money()
-new_player.betting()
-print(repr(new_player))
-
-'''
-player2 = Player("player_2", 0)
-player2.add_money()
-print(repr(player2))
-'''
+    def get_hand_value(self):
+        return self.hand.calculate_value()
