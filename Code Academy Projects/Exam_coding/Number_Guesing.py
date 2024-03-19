91a06eb1968cd73c757e902b6de222621e703898
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-19 13:45:49 -0400

    cleaned up the code and added notes

--- Code Academy Projects/Exam_coding/Number_Guesing.py
+++ Code Academy Projects/Exam_coding/Number_Guesing.py
@@ -1,46 +1,49 @@
-# Import random class
+# Importing randint function from the random module
 from random import randint
 
 class Number_Guesser:
   
   def __init__(self, player_names):
+    # Initialize an empty dictionary to store player guesses
     self.player_guesses = {}
-
-
-    # Adds names and -1 to player_guesses
+    
+    # Initialize secret_number with a random number between 1 and 10
+    self.secret_number = randint(1,10)
+    
+    # Add player names to player_guesses with initial guess as -1
     for name in player_names:
       self.player_guesses[name] = -1
-      
-    # Update to choose a random number
-      self.secret_number = randint(1,10)
-
 
 #####################################################
   def add_player_guess(self, name, guess):
-    # Fill in this method
+    # Method to add player guesses
     if name in self.player_guesses:
-      print(name," has guesed ",guess)
-        
-    
-    
+      self.player_guesses[name] = guess  # Update the guess for the player
+      print(name," has guessed ",guess)
 ######################################################     
+
   def print_answer(self):
+    # Method to print the secret number
     print(str(self.secret_number), "is the secret number!")
     
   def print_guesses(self):
-    for player in self.player_guesses.items():
-      if player[1] != -1:
-        print(player[0], "guessed", str(player[1]))
+    # Method to print player guesses
+    for player, guess in self.player_guesses.items():
+      if guess != -1:
+        print(player, "guessed", str(guess))
       else:
-        print(player[0], "needs to guess!") 
+        print(player, "needs to guess!") 
 
 
+# Create an instance of Number_Guesser
 game1 = Number_Guesser(["Thuy", "Joe", "Diya"])
 
+# Add player guesses
 game1.add_player_guess("Roger", 10)
 game1.add_player_guess("Diya", 8)
 game1.add_player_guess("Thuy", 1)
 game1.add_player_guess("Joe", 5)
 
+# Print player guesses and the secret number
 game1.print_guesses()
 game1.print_answer()
