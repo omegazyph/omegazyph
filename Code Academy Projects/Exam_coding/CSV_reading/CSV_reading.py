25cd9119424e8425a86b2f7a7b18a9cd16b57948
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-19 14:39:26 -0400

    fixed up the csv file and mod the csv_reading file

--- Code Academy Projects/Exam_coding/CSV_reading/CSV_reading.py
+++ Code Academy Projects/Exam_coding/CSV_reading/CSV_reading.py
@@ -1,6 +1,6 @@
 # Fill in your code below
 import csv
-with open("employees.csv", "r") as handler:
+with open("Code Academy Projects/Exam_coding/CSV_reading/employees.csv", "r") as handler:
  reader = csv.reader(handler, delimiter=',')
  for row in reader:
    print(row)
