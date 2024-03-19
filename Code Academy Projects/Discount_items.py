bd7ded075a1b091f234672f27059c557bd2b04c0
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-19 12:22:04 -0400

    added diacounted items

---
+++ Code Academy Projects/Discount_items.py
@@ -0,0 +1,12 @@
+# All of our store items
+all_items = [["Taffy", 1], ["Chocolate", 2], ["Cup", 5], ["Plate", 10], ["Bowl", 11], ["Silverware", 22]]
+
+# Empty discounted_items list
+discounted_items = []
+
+# Your code here
+for items in all_items:
+  if 
+
+# For testing purposes: print discounted list
+print(discounted_items)
