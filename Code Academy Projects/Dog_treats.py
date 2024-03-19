cbc76b96c75e351906752e9fa3bc5a4aceebdbd4
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-19 12:39:13 -0400

    added conetnet to buy_items

--- Code Academy Projects/Dog_treats.py
+++ Code Academy Projects/Dog_treats.py
@@ -1,13 +1,27 @@
+def buy_items(starting_money, starting_num_items, item_price):
+    # Initialize a variable to keep track of the number of items bought
+    num_bought = 0
+    
+    # Continue buying items until either there is no more money or no more items left to buy
+    while starting_money >= item_price and starting_num_items > 0:
+        # Deduct the price of the item from the available money
+        starting_money -= item_price
+        
+        # Decrement the number of available items by 1
+        starting_num_items -= 1
+        
+        # Increment the number of items bought
+        num_bought += 1
+    
+    # Return the total number of items bought
+    return num_bought
+
+# Initial values
 starting_money = 100
 starting_num_items = 10
 item_price = 4
 
-# Your code here
-def buy_items():
-  num_bought = 0
-  
-  return 0
-
+# Call the function and store the result in 'total'
 total = buy_items(starting_money, starting_num_items, item_price)
 print("You were able to buy " + str(total) + " items.")
   
