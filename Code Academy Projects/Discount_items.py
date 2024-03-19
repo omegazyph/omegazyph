4905d5a82c170bff96e064445cc9a2a285926ae6
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-19 12:28:31 -0400

    Added a for loop to loop thought the list to find odd prises and print out the names

--- Code Academy Projects/Discount_items.py
+++ Code Academy Projects/Discount_items.py
@@ -5,8 +5,9 @@ all_items = [["Taffy", 1], ["Chocolate", 2], ["Cup", 5], ["Plate", 10], ["Bowl",
 discounted_items = []
 
 # Your code here
-for items in all_items:
-  if 
+for item in all_items:
+  if item[1] % 2 != 0:
+    discounted_items.append(item[0])
 
 # For testing purposes: print discounted list
 print(discounted_items)
