8e3f9d71ca43053874279309996e7e344c1b9c4b
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-19 12:32:03 -0400

    added dog_treats.py

---
+++ Code Academy Projects/Dog_treats.py
@@ -0,0 +1,18 @@
+starting_money = 100
+starting_num_items = 10
+item_price = 4
+
+# Your code here
+def buy_items():
+  num_bought = 0
+  
+  return 0
+
+total = buy_iems(starting_money, starting_num_items, item_price)
+print("You were able to buy " + str(total) + " items.")
+  
+# For testing purposes
+total_1 = buy_items(100, 10, 4)
+print("Test 1: " + str(total_1))
+total_2 = buy_items(10, 10, 4)
+print("Test 2: " + str(total_2))
