4c04ecf6c491f2797455c0f8c7357916a1beb967
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-19 12:55:17 -0400

    fixed the if statements

--- Code Academy Projects/hash_tag_gen.py
+++ Code Academy Projects/hash_tag_gen.py
@@ -1,23 +1,25 @@
 class HashtagsCreator:
   
-  def __init__(self,list_of_terms):
+  def __init__(self, list_of_terms):
     self.hashtags = []
     
+    # Iterate through each term in the list_of_terms
     for term in list_of_terms:
-      # Fix this section of code
+      # If term contains '@', replace '@' with '#' and add to hashtags list
       if '@' in term:
         self.hashtags.append(term.replace('@', '#'))
-        if '#' in term:
-          self.hashtags.append(term)
-          if not '#' or '@' in term:
-            self.hashtags.append('#' + term)
-          else:
-            self.hashtags.append(term.replace(term[:1], '#'))
+      # If term contains '#', just add the term to hashtags list
+      elif '#' in term:
+        self.hashtags.append(term)
+      # If neither '@' nor '#' is present in term, prepend '#' to the term and add to hashtags list
+      else:
+        self.hashtags.append('#' + term)
       
   def list_hashtags(self):
+    # Iterate through each hashtag in self.hashtags and print it
     for hashtag in self.hashtags:
       print(hashtag)
 
-# Do not edit testing code
+# Testing code
 test_hashtags = HashtagsCreator(["@codecademy", "#python", "programming", "#strings"])
 test_hashtags.list_hashtags()
