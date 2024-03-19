361ce3966e7d448d8136201fe4aa83559ecf4c56
omegazyph <156851487+omegazyph@users.noreply.github.com>
2024-03-19 14:52:58 -0400

    chengeed the input file

--- Code Academy Projects/Exam_coding/CSV_reading/CSV_reading.py
+++ Code Academy Projects/Exam_coding/CSV_reading/CSV_reading.py
@@ -1,7 +1,7 @@
 # Fill in your code below
 import csv
-with open("Code Academy Projects/Exam_coding/CSV_reading/employees.csv", "r") as handler:
- reader = csv.reader(handler, delimiter=',')
- for row in reader:
-   print(row)
 
+input_file = csv.DictReader(open("Code Academy Projects/Exam_coding/CSV_reading/employees.csv"))
+reader = csv.reader(input_file, delimiter=',')
+for row in input_file:
+   print(row)
