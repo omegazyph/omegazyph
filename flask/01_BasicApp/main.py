b83b3c9f01ce4f326f2ffa0b5fb5412d6a4504fc
omegazyph <omegazyph@gmail.com>
2024-04-13 14:08:50 -0400

    added comment and del lines of code so just the home page is there

--- flask/01_BasicApp/main.py
+++ flask/01_BasicApp/main.py
@@ -10,20 +10,7 @@ app = Flask(__name__)
 def index():
     return 'This is the home page'
 
-@app.route('/tuna')
-def tuna():
-    return'<h2>Tuna is good</h2>'
 
-
-@app.route('/profile/<username>')
-def profile(username):
-    return "Hey there %s" %username
-
-@app.route('/post/<int:post_id>')
-def post(post_id):
-    return "<h2>Post ID is %s</h2>" %post_id
-
-
-# to start the sever
+# to start the app on 127.0.0.1:5000
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
