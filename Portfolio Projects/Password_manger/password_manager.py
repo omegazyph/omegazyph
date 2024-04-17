46c551d80f3949823109b957d4037d4cce4a8fd9
omegazyph <omegazyph@gmail.com>
2024-04-17 10:56:20 -0400

    added comments

--- Portfolio Projects/Password_manger/password_manager.py
+++ Portfolio Projects/Password_manger/password_manager.py
@@ -1,6 +1,8 @@
 from cryptography.fernet import Fernet
 
 print("do not use | ")
+
+# Function to generate and write a new key to a file
 ''' run if need a Key
 def write_key():
     key = Fernet.generate_key()
@@ -9,24 +11,23 @@ def write_key():
         print ('wrote a new new')
 write_key()'''
 
-
+# Function to load the key from the file
 def load_key():
     file = open("key.key", 'rb')
     key = file.read()
     file.close()
     return key 
 
-
+# Asking for the master password and loading the key
 master_pwd = input("What is the master password? :> ")
 key = load_key()
 fer = Fernet(key)
 
-
-
+# Function to create a password (not implemented)
 def create_pwd():
     pass
 
-
+# Function to view existing passwords
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
@@ -34,6 +35,7 @@ def view():
             user, passw = data.split("|")
             print("User:",user, "| Password:", fer.decrypt(passw.encode()).decode())
 
+# Function to add a new password
 def add():
     name = input("Account Name :> ")
     pwd = input("Password :> ")
@@ -41,7 +43,7 @@ def add():
     with open('passwords.txt', 'a') as f:
         f.write(name + '|' + fer.encrypt(pwd.encode()).decode() + "\n")
 
-
+# Main loop for interacting with the user
 while True:
     mode = input("Would you like to add a new password or view existing ones (view, add), press Q to Quit:> ").lower()
     if mode == "q":
