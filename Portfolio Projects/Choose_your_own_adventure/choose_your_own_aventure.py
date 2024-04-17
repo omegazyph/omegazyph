8af33adc2b3eb8a29d24a41349db4aa36f9ac370
omegazyph <omegazyph@gmail.com>
2024-04-16 22:06:53 -0400

    working on the if statment

--- Portfolio Projects/Choose_your_own_adventure/choose_your_own_aventure.py
+++ Portfolio Projects/Choose_your_own_adventure/choose_your_own_aventure.py
@@ -1,4 +1,12 @@
 name = input("Type your name :> ")
 print("Welcome",name, "to this aventure!")
 
-answer = input("")
+answer = input("You are on a dirt road, it has come to an end and you can go left or right.  Which way would you like to go? :> ").lower()
+
+if answer == "left":
+    answer = input("You come tok a river, you can walk around it or swim across? Type walk to walk around or swim to swim across? :> ")
+elif answer == "Right":
+    print()
+
+else:
+    print("Not a vaild option. You lose.")
