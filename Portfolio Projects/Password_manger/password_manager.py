df7dc050891d61536a1c92551e7200ed924915fa
omegazyph <omegazyph@gmail.com>
2024-04-17 10:43:18 -0400

    del the key to make another

--- Portfolio Projects/Password_manger/password_manager.py
+++ Portfolio Projects/Password_manger/password_manager.py
@@ -1,20 +1,22 @@
 from cryptography.fernet import Fernet
 
 print("do not use | ")
+''' run if need a Key
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", 'wb') as key_file:
+        key_file.write(key)'''
 def load_key():
     file = open("key.key", 'rb')
     key = file.read()
     file.close()
     return key 
 
-key = load_key()
-master_pwd = input("What is the master password? :> ")
 
-''' run if need a Key
-def write_key():
-    key = Fernet.generate_key()
-    with open("key.key", 'wb') as key_file:
-        key_file.write(key)'''
+master_pwd = input("What is the master password? :> ")
+key = load_key()
+fer = Fernet(key)
+
 
 
 def create_pwd():
@@ -26,14 +28,14 @@ def view():
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print("User:",user, "| Password:", passw)
+            print("User:",user, "| Password:", fer.decrypt(passw.encode()).decode())
 
 def add():
     name = input("Account Name :> ")
     pwd = input("Password :> ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(name + '|' + pwd + "\n")
+        f.write(name + '|' + fer.encrypt(pwd.encode()).decode() + "\n")
 
 
 while True:
