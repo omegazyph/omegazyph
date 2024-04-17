3f97559e8f8eb711438c984ab6c6d77bfa997aa7
omegazyph <omegazyph@gmail.com>
2024-04-17 11:25:39 -0400

    bring it back

--- Portfolio Projects/Password_manger/password_manager.py
+++ Portfolio Projects/Password_manger/password_manager.py
@@ -2,50 +2,48 @@ from cryptography.fernet import Fernet
 
 print("do not use | ")
 
-class PasswordManager:
-    def __init__(self) -> None:
-        # Asking for the master password and loading the key
-        self.master_pwd = input("What is the master password? :> ")
-        self.key = self.load_key()
-        self.fer = Fernet(self.key)
+class Password_manager:
 
     # Function to generate and write a new key to a file
     # don't call this function until you need a Key
-    @staticmethod
     def write_key():
         key = Fernet.generate_key()
         with open("key.key", 'wb') as key_file:
             key_file.write(key)
-        print('Wrote a new key')
+            print ('wrote a new new')
+
 
     # Function to load the key from the file
-    @staticmethod
     def load_key():
-        with open("key.key", 'rb') as key_file:
-            key = key_file.read()
+        file = open("key.key", 'rb')
+        key = file.read()
+        file.close()
         return key 
 
+    # Asking for the master password and loading the key
+    master_pwd = input("What is the master password? :> ")
+    key = load_key()
+    fer = Fernet(key)
+
     # Function to create a password (not implemented)
-    @staticmethod
     def create_pwd():
         pass
 
     # Function to view existing passwords
-    @staticmethod
     def view():
         with open('passwords.txt', 'r') as f:
-            for line in f:
-                user, passw = line.strip().split("|")
-                print("User:", user, "| Password:", PasswordManager.fer.decrypt(passw.encode()).decode())
+            for line in f.readlines():
+                data = line.rstrip()
+                user, passw = data.split("|")
+                print("User:",user, "| Password:", fer.decrypt(passw.encode()).decode())
 
     # Function to add a new password
-    @staticmethod
     def add():
         name = input("Account Name :> ")
         pwd = input("Password :> ")
 
         with open('passwords.txt', 'a') as f:
-            f.write(name + '|' + PasswordManager.fer.encrypt(pwd.encode()).decode() + "\n")
+            f.write(name + '|' + fer.encrypt(pwd.encode()).decode() + "\n")
 
 # Main loop for interacting with the user
 while True:
@@ -54,9 +52,9 @@ while True:
         break
 
     if mode == "view":
-        PasswordManager.view()
+        Password_manager.view()
     elif mode == "add":
-        PasswordManager.add()
+        Password_manager.add()
     else:
         print("Invalid selection")
         continue
