b5303a3521d83220c0d88c135b939c442574db76
omegazyph <omegazyph@gmail.com>
2024-04-29 21:33:43 -0400

    clean it up tha added comments

--- Turtle graphics/249.py
+++ Turtle graphics/249.py
@@ -1,14 +1,29 @@
+# this is a cicle in colors
+# Wayne Stock
+# date: 2024-04-29 
+
+
 import turtle as t
 
+# Set background color to black
 t.bgcolor("black")
+
+# Set the drawing speed to the fastest
 t.speed(0)
+
+# Set the pen size to 3
 t.pensize(3)
 
+# Loop to draw the circles
 for i in range(200):
+    # Loop to change colors after drawing each circle
     for color in ("Deeppink","Lime","Red"):
+        # Set the pen color to the current color in the loop
         t.color(color)
+        # Draw a circle with radius decreasing as i increases
         t.circle(100-i)
+        # Turn the turtle left by 20 degrees
         t.left(20)
 
-t.draw()
+# Keep the window open until it's manually closed
 t.done()
