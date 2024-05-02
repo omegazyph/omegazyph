d47fb57a434cd75945d42b4cef3bf9bfd4f3b908
omegazyph <omegazyph@gmail.com>
2024-05-01 22:08:14 -0400

    added story 2 need to fix errors

--- Playing_around/Mad_Libs_Generator_Game.py
+++ Playing_around/Mad_Libs_Generator_Game.py
@@ -74,7 +74,40 @@ def Story1(win):
 #endregion
 
 
-
+def Story2(win):
+def final(tl: Toplevel, profession, noun, feeling, emotion,verb):
+            text = f'''
+            One day me and my friend {name} decided to play a {sports} game in {City}.
+       But we were not able to play. So, we went to the game and watched our favourite player {playername}.
+       We drank {drinkname} and also ate some {snacks} 
+       We really enjoyed it! We are looking forward to go again and enjoy 
+'''
+ 
+            tl.geometry(newGeometry='500x550')
+ 
+            Label(tl, text='Story:',  wraplength=tl.winfo_width()).place(x=160, y=310)
+            Label(tl, text=text,wraplength=tl.winfo_width()).place(x=0, y=330)
+    NewScreen = Toplevel(win, bg='red')
+    NewScreen.title("Ambitions")
+    NewScreen.geometry('500x500')
+    Label(NewScreen, text='Ambitions').place(x=150, y=0)
+    Label(NewScreen, text='Enter a profession:').place(x=0, y=35)
+    Label(NewScreen, text='Enter a noun:').place(x=0, y=70)
+    Label(NewScreen, text='Enter a feeling:').place(x=0, y=110)
+    Label(NewScreen, text='Enter a emotion:').place(x=0, y=150)
+    Label(NewScreen, text='Enter a verb:').place(x=0, y=190)
+    Profession = Entry(NewScreen, width=17)
+    Profession.place(x=250, y=35)
+    Noun = Entry(NewScreen, width=17)
+    Noun.place(x=250, y=70)
+    Feeling = Entry(NewScreen, width=17)
+    Feeling.place(x=250, y=105)
+    Emotion= Entry(NewScreen, width=17)
+    Emotion.place(x=250, y=150)
+    Verb = Entry(NewScreen, width=17)
+    Verb.place(x=250, y=190)
+    SubmitButton = Button(NewScreen, text="Submit", background="Blue", font=('Times', 12), command=lambda:final(NewScreen, Profession.get(), Noun.get(), Feeling.get(), Emotion.get(), Verb.get()))
+    SubmitButton.place(x=150, y=270)
 
 
 
