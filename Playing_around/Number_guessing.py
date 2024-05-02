c70db88d9a558192273d0fe36cd37b14d67ae1a8
omegazyph <omegazyph@gmail.com>
2024-05-02 19:19:44 -0400

    added comments cleaned it up and added aheader

--- Playing_around/Number_guessing.py
+++ Playing_around/Number_guessing.py
@@ -1,19 +1,30 @@
+"""
+Number Guessing Game
+by Wayne Stock
+created on 2024-05-02
+
+This program allows the user to play a number guessing game. The user has to guess a randomly generated number
+within a specified range, and they have three attempts to guess it correctly.
+
+"""
+
 import random
 
 def number_guessing_game():
+    # Welcome message
     print("Welcome to the Number Guessing Game!")
-    print("you only have 3 try's")
-    
+    print("You have only 3 tries.")
+
     # Set the range for the random number (e.g., between 1 and 100)
     lower_limit = 1
     upper_limit = 100
     secret_number = random.randint(lower_limit, upper_limit)
 
-    # Initialize the player's guess
+    # Initialize variables
     guess = None
-    number_of_trys = 0
-    
+    number_of_tries = 0
 
+    # Game loop
     while guess != secret_number:
         try:
             # Get the player's guess
@@ -22,18 +33,16 @@ def number_guessing_game():
             # Provide hints
             if guess < secret_number:
                 print("Too low! Try again.")
-                number_of_trys +=1
-                
-
+                number_of_tries += 1
             elif guess > secret_number:
                 print("Too high! Try again.")
-                number_of_trys +=1
-                
+                number_of_tries += 1
             else:
                 print(f"Congratulations! You guessed the correct number: {secret_number}")
 
-            if number_of_trys == 3:
-                print("Start over and try again")
+            # Check if the user has reached the maximum number of tries
+            if number_of_tries == 3:
+                print("You've reached the maximum number of tries. Start over and try again.")
                 break
 
         except ValueError:
