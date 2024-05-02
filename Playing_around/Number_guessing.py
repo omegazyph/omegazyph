827cbc3a8ab4432e365d19198deea3974845879e
omegazyph <omegazyph@gmail.com>
2024-05-02 19:16:41 -0400

    added a function to only have 3 trys

--- Playing_around/Number_guessing.py
+++ Playing_around/Number_guessing.py
@@ -2,7 +2,8 @@ import random
 
 def number_guessing_game():
     print("Welcome to the Number Guessing Game!")
-
+    print("you only have 3 try's")
+    
     # Set the range for the random number (e.g., between 1 and 100)
     lower_limit = 1
     upper_limit = 100
@@ -10,19 +11,31 @@ def number_guessing_game():
 
     # Initialize the player's guess
     guess = None
+    number_of_trys = 0
+    
 
     while guess != secret_number:
         try:
             # Get the player's guess
-            guess = int(input(f"Guess the number between {lower_limit} and {upper_limit}: "))
+            guess = int(input(f"Guess the number between {lower_limit} and {upper_limit}\n<: "))
 
             # Provide hints
             if guess < secret_number:
                 print("Too low! Try again.")
+                number_of_trys +=1
+                
+
             elif guess > secret_number:
                 print("Too high! Try again.")
+                number_of_trys +=1
+                
             else:
                 print(f"Congratulations! You guessed the correct number: {secret_number}")
+
+            if number_of_trys == 3:
+                print("Start over and try again")
+                break
+
         except ValueError:
             print("Please enter a valid number.")
 
