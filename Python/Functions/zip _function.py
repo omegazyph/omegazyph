e4394942adf0bca82939b8cdd63075607bffa55f
omegazyph <omegazyph@gmail.com>
2024-06-13 16:18:07 -0500

    added comments and added the unzip funtion

--- Python/Functions/zip _function.py
+++ Python/Functions/zip _function.py
@@ -5,11 +5,26 @@ from itertools import zip_longest
 
 list1 = [1,2,3]
 list2 = ['a', 'b']
+list3 = ['True', 'False', 'True']
 
+print("zipped lists")
 # this will not pick up the 3 value
-zipped = zip(list1, list2)
+zipped = zip(list1, list2, list3)
+print(list(zipped))
+print("\n")
+
+print("added empty value")
+# This will pick up the 3rd value
+zipped = zip_longest(list1, list2, list3, fillvalue='N/A')
 print(list(zipped))
 
-# This will pick up the 3rd value
-zipped = zip_longest(list1, list2, fillvalue='N/A')
-print(list(zipped))
+
+print("\n")
+print("unzipped lists")
+# to unzip the lists
+zipped = [(1, 'a', 'True'), (2, 'b', 'False'), (3, 'N/A', 'True')]
+unzipped = zip(*zipped)
+
+print(list1)
+print(list2)
+print(list3)
