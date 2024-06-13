f2076e2677057ccf1beef0a6dcf05a8c2de5bda3
omegazyph <omegazyph@gmail.com>
2024-06-13 16:41:27 -0500

    updated the notes

--- Python/Eval.py
+++ Python/Eval.py
@@ -1,6 +1,6 @@
 '''this is a secunity risk because a
-person and typ somthing like print('got hacked')
-and it will exacete'''
+a person can input a malicious script
+and it will get executed'''
 
 
 print('\n')
