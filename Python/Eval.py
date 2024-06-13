884ac335dba87ce5021c1412d429a781e66016cb
omegazyph <omegazyph@gmail.com>
2024-06-13 16:39:30 -0500

    coded eval notes

---
+++ Python/Eval.py
@@ -0,0 +1,10 @@
+'''this is a secunity risk because a
+person and typ somthing like print('got hacked')
+and it will exacete'''
+
+
+print('\n')
+expression = input('Enter an arithmetic expression: ')
+
+result = eval(expression)
+print("Result is: ", result)
