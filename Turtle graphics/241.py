c287c00dd359839f10a43b4b48654d2b7896f65f
omegazyph <omegazyph@gmail.com>
2024-07-07 16:03:15 -0500

    fixed the ruff errors

--- Turtle graphics/241.py
+++ Turtle graphics/241.py
@@ -1,20 +1,28 @@
-#2024-07-07
-from turtle import *
-from colorsys import *
-bgcolor("black")
+from turtle import Screen, Turtle
+from colorsys import hsv_to_rgb
 
-speed(0)
-h = 0
-goto(120, -30)
+def draw_pattern(turtle):
+    h = 0
+    turtle.goto(120, -30)
+    for _ in range(8):
+        turtle.color(hsv_to_rgb(h, 1, 1))
+        for _ in range(75):
+            h += 0.0115
+            turtle.forward(90)
+            turtle.backward(90)
+            turtle.right(3)
+        turtle.forward(100)
 
-for x in range(8):
-    color(hsv_to_rgb(h, 1, 1))
-    for j in range(75):
-        h += 0.0115  # Corrected line
-        forward(90)
-        backward(90)
-        right(3)
+def main():
+    screen = Screen()
+    screen.bgcolor("black")
 
-    forward(100)
+    turtle = Turtle()
+    turtle.speed(0)
 
-done()
+    draw_pattern(turtle)
+
+    screen.mainloop()
+
+if __name__ == "__main__":
+    main()
