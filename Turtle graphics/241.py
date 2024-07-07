0392b7f3e7eb02c5a5812bc3af16e3055f0d4f56
omegazyph <omegazyph@gmail.com>
2024-07-07 15:57:38 -0500

    fixed the hue

--- Turtle graphics/241.py
+++ Turtle graphics/241.py
@@ -1,19 +1,19 @@
-from turtle  import *
+from turtle import *
 from colorsys import *
 bgcolor("black")
 
 speed(0)
-h=0
+h = 0
 goto(120, -30)
 
 for x in range(8):
-    color(hsv_to_rgb(h,1,1))
+    color(hsv_to_rgb(h, 1, 1))
     for j in range(75):
-        h+0.0115
+        h += 0.0115  # Corrected line
         forward(90)
         backward(90)
         right(3)
 
-    forward(250)
+    forward(100)
 
 done()
