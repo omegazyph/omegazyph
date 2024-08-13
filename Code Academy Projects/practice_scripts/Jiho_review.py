8ce1229be2571010daa8c10e118f52d9dc820a06
omegazyph <omegazyph@gmail.com>
2024-08-13 12:46:20 -0400

    added impovements

--- Code Academy Projects/practice_scripts/Jiho_review.py
+++ Code Academy Projects/practice_scripts/Jiho_review.py
@@ -19,10 +19,16 @@ tables = {
 #user setup
 guest_name = input('What is your name: ')
 guest_table_number = int(input("What's the table number: "))
+
+
 def assign_table(table_number, name, vip_status=False): 
-  tables[table_number]['name'] = name
-  tables[table_number]['vip_status'] = vip_status
-  tables[table_number]['order'] = {}
+  if table_number in tables:
+     tables[table_number]['name'] = name
+     tables[table_number]['vip_status'] = vip_status
+     tables[table_number]['order'] = {}
+  else:
+     print("Invalid table number.")
+
 assign_table(guest_table_number,guest_name)
 #print(tables[guest_table_number])
 
@@ -33,11 +39,14 @@ assign_table(guest_table_number,guest_name)
 # Table order
 guest_drinks = input("What would they like to drink: ")
 guest_food = input("What would they like to eat: ")
+
 def assign_food_items(table_number, **order_items):
-  food = order_items.get('food')
-  drinks = order_items.get('drinks')
-  tables[table_number]['order']['food_items'] = food
-  tables[table_number]['order']['drinks'] = drinks
+  if table_number in tables:
+     food = order_items.get('food')
+     drinks = order_items.get('drinks')
+     tables[table_number]['order']['food_items'] = food
+     tables[table_number]['order']['drinks'] = drinks
+
 assign_food_items(guest_table_number, 
                   food=[guest_food], 
                   drinks=[guest_drinks])
@@ -45,29 +54,23 @@ assign_food_items(guest_table_number,
 
 
 
-
-
-
-
-
-
-
-
-
 # Split the order
-charge = int(float(input("What iis the charge of the meal: ")))
-guest_tip = int(float(input("What is the agreed amont for a tip: ")))
-people = int(input("how many peolpe in the group: "))
+try:
+   charge = int(float(input("What iis the charge of the meal: ")))
+   guest_tip = int(float(input("What is the agreed amont for a tip: ")))
+   people = int(input("how many peolpe in the group: "))
 
 
-list1= [charge, guest_tip, people]
-tables[guest_table_number]['order']['total']= list1
+   list1= [charge, guest_tip, people]
+   tables[guest_table_number]['order']['total']= list1
 
-def calculate_price_per_person(total, tip, split):
-    total_tip = total * (tip/100)
-    split_price = (total + total_tip) / split
-    print('split price',split_price,"per person")
+   def calculate_price_per_person(total, tip, split):
+      total_tip = total * (tip/100)
+      split_price = (total + total_tip) / split
+      print('split price',split_price,"per person")
 
-total, tip, split = tables[guest_table_number]['order']['total']
-calculate_price_per_person(total, tip, split)
+   total, tip, split = tables[guest_table_number]['order']['total']
+   calculate_price_per_person(total, tip, split)
 
+except ValueError:
+   print("Invalid input.  PLease enter numeric values for charge, tip and people.")
