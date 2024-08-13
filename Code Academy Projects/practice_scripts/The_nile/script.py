5f5d29af3c2d0baa840d7a5486c09a835828955c
omegazyph <omegazyph@gmail.com>
2024-08-13 14:11:47 -0400

    inital save for the nile project

---
+++ Code Academy Projects/practice_scripts/The_nile/script.py
@@ -0,0 +1,20 @@
+from nile import get_distance, format_price, SHIPPING_PRICES
+from test import test_function
+
+# Define calculate_shipping_cost() here:
+
+
+# Test the function by calling 
+# test_function(calculate_shipping_cost)
+
+# Define calculate_driver_cost() here
+
+
+# Test the function by calling 
+# test_function(calculate_driver_cost)
+
+# Define calculate_money_made() here
+
+
+# Test the function by calling 
+# test_function(calculate_money_made)
