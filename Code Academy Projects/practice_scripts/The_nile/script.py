d79586367bd01dd7843acb3f1cfd24387e15f530
omegazyph <omegazyph@gmail.com>
2024-08-14 10:28:07 -0400

    finished calculate_shipping_cost

--- Code Academy Projects/practice_scripts/The_nile/script.py
+++ Code Academy Projects/practice_scripts/The_nile/script.py
@@ -2,18 +2,20 @@ from nile import get_distance, format_price, SHIPPING_PRICES
 from test import test_function
 
 # Define calculate_shipping_cost() here:
-def calulate_shipping_cost(from_coords, 
+def calculate_shipping_cost(from_coords, 
                            to_coords, 
-                           shipping_type):
+                           shipping_type = 'Overnight'):
     from_lat,from_long = from_coords
     to_lat, to_long = to_coords
     distance = get_distance(*from_coords, *to_coords)
-
+    cost = SHIPPING_PRICES[shipping_type]
+    price = distance * cost
+    return format_price(price)
 
 
 
 # Test the function by calling 
-# test_function(calculate_shipping_cost)
+test_function(calculate_shipping_cost)
 
 # Define calculate_driver_cost() here
 
