41bbab152824732d611499bca5bc2b2423d6a25c
omegazyph <omegazyph@gmail.com>
2024-08-14 12:19:25 -0400

    finsished calculate_driver_cost ans tested it

--- Code Academy Projects/practice_scripts/The_nile/script.py
+++ Code Academy Projects/practice_scripts/The_nile/script.py
@@ -11,17 +11,36 @@ def calculate_shipping_cost(from_coords,
     cost = SHIPPING_PRICES[shipping_type]
     price = distance * cost
     return format_price(price)
-
-
-
 # Test the function by calling 
-test_function(calculate_shipping_cost)
+#test_function(calculate_shipping_cost)
+
+
 
 # Define calculate_driver_cost() here
+def calculate_driver_cost(distance,*drivers):
+    cheapest_driver = None
+    cheapest_driver_price = None
 
+    for driver in drivers:
+        driver_time = distance / driver.speed
+        price_for_driver = driver.salary * driver_time
+        if cheapest_driver is None:
+            cheapest_driver = driver
+            cheapest_driver_price = price_for_driver
+        elif price_for_driver < cheapest_driver_price:
+            cheapest_driver = driver
+            cheapest_driver_price = price_for_driver
+    return cheapest_driver_price, cheapest_driver           
 
 # Test the function by calling 
-# test_function(calculate_driver_cost)
+#test_function(calculate_driver_cost)
+
+
+
+
+
+
+
 
 # Define calculate_money_made() here
 
