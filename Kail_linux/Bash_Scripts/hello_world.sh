796f4d3d01592047d8ebda0ec30ef0ee4236ff1f
omegazyph <omegazyph@gmail.com>
2024-08-14 13:07:36 -0400

    clean up the code and added comments

--- Kail_linux/Bash_Scripts/hello_world.sh
+++ Kail_linux/Bash_Scripts/hello_world.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo Hello World!
+# Bash Script hello world
+# Author: Wayne Stock
+# Date: 2024-01-13
+# Description: This is a simple bash script
 
+# Print a simple greeting message to the terminal
+echo "Hello, World!"
