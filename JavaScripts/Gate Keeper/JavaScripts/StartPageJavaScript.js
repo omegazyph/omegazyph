2641d79c287a939a09b1755054cda0b7ab54d80f
omegazyph <omegazyph@gmail.com>
2025-12-29 19:11:17 -0600

    Updated the gateKeeper and added README.md

--- JavaScripts/Gate Keeper/JavaScripts/StartPageJavaScript.js
+++ JavaScripts/Gate Keeper/JavaScripts/StartPageJavaScript.js
@@ -1,6 +1,14 @@
-﻿var nifty_little_window = null;
+﻿/************************************************************
+ * PROJECT: Gate Keeper
+ * FILE: StartPageJavaScript.js
+ * DESCRIPTION: Launches the secure popup challenge window.
+ * AUTHOR: [Omegazyph]
+ * DATE: (updated December 2025)
+ ************************************************************/
 
-function gateKeeper()
-{
+var nifty_little_window = null;
+
+function gateKeeper() {
+    // Opens a new specialized window for the password entry
     nifty_little_window = window.open('gatekeep.html', 'theKeeper', 'width=350,height=200,resizable=1');
 }
--- JavaScripts/Gate Keeper/JavaScripts/gatekeeperJavaScript.js
+++ JavaScripts/Gate Keeper/JavaScripts/gatekeeperJavaScript.js
@@ -1,24 +1,38 @@
-﻿
-function goForit()
-{
+﻿/************************************************************
+ * PROJECT: Gate Keeper
+ * FILE: gatekeeperJavaScript.js
+ * DESCRIPTION: Validates access by attempting to load a file
+ * named after the user's password input.
+ * AUTHOR: [Omeagzyph]
+ * DATE: (updated December 2025)
+ ************************************************************/
+
+function goForit() {
     var location;
     var password;
+
+    // Grabs the text from the input box in 'testform'
     password = this.document.testform.inputbox.value;
+    
+    // The password IS the filename (e.g., "Guest.html")
     location = password + ".html";
+    
     fetch(location);
-    theKeeper = window.close();
+    
+    // Closes the small pop-up window after submitting
+    window.close();
 }
 
-function fetch(location)
-{
+function fetch(location) {
     var root;
-    if (opener.closed)
-    {
+
+    // Checks if the main "Start Page" (the opener) is still open
+    if (opener.closed) {
+        // If the main window was closed, open a brand new one
         root = window.open('', 'theKeepersGopher', 'toolbar=yes,location=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,copyhistory=no');
         root.location.href = location;
-    }
-    else
-    {
+    } else {
+        // If the main window is still there, load the new page inside it
         opener.location.href = location;
     }
 }
