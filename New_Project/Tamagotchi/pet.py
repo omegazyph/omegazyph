dd9f4f2922c697fe5aa6235e92bb84e8accba5de
omegazyph <omegazyph@gmail.com>
2026-01-09 14:10:42 -0600

    added a class to pet.py

--- New_Project/Tamagotchi/pet.py
+++ New_Project/Tamagotchi/pet.py
@@ -1,18 +1,25 @@
-###############################################################################
 # Date: 2026-01-09
 # Script Name: pet.py
 # Author: omegazyph
 # Updated: 2026-01-09
 # Description: A logic-based virtual pet (Tamagotchi clone) 
 # that tracks hunger, happiness, and health.
-###############################################################################
 
+class VirtualPet:
+    def __init__(self, name):
+        # The name the user gives the pet
+        self.name = name
+        
+        # 0 is full, 100 is starving
+        self.hunger = 50
+        
+        # 100 is happy, 0 is sad
+        self.happiness = 50
+        
+        # We track if the pet is still alive/active
+        self.is_alive = True
+        
+        print(f"Aww! {self.name} has been born!")
 
-class pet:
-    def __init__(self):
-        # this means the pet is full
-        self.hunger = 0
-        # this means joyfull
-        self.happiness = 100
-
-        pass
+# This is where we will create the pet object later
+# my_pet = VirtualPet("Buddy")
