920a5eddeb60caf1172c0864ab80faa5935eb219
omegazyph <omegazyph@gmail.com>
2026-09-07 20:14:45 -0500

    created a assii header

---
+++ Scripts/Python_Scripts/asii_headers/asii_header.py
@@ -0,0 +1,12 @@
+#################################################
+# date: 2026-09-07
+# Script Name: asii_header.py
+# Author: Wayne Stock
+# Updated: 2026-09-07
+# Discription: prints assiii to the screen
+################################################
+
+# need to install pyfiglet
+from pyfiglet import figlet_format
+
+print(figlet_format("The Professor"))
