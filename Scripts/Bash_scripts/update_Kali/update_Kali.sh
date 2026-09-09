f87c5b4f1f47d001fec1a78e86ca6ec1ab9a3156
omegazyph <omegazyph@gmail.com>
2026-09-09 13:43:52 -0500

    added a function to check if the user uses sudo

--- Scripts/Bash_scripts/update_Kali/update_Kali.sh
+++ Scripts/Bash_scripts/update_Kali/update_Kali.sh
@@ -12,6 +12,12 @@
 # Exit immediately if any command fails (for unattend automation)
 set -e
 
+# Ensure script is executed with root privilages
+if [ "$EUID" -ne 0 ]; then
+    echo "Error: This script must be run as root."
+    exit 1
+fi
+
 echo "==================================================="
 echo "      Starting Kali Linux System Maintenance."
 echo "==================================================="
@@ -20,34 +26,34 @@ echo "==================================================="
 
 # 1. Check for available updates for your package lists.
 echo -e "Checking for updates..."
-sudo apt-get update -y
+apt-get update -y
 
 # 2. Full Distribut Upgrade
 echo -e "\nUpgrading all packages, including Bash..."
-sudo apt-get dist-upgrade -y
+apt-get dist-upgrade -y
 
 # 3. Install and update the Exploit Database package.
 echo -e "\nInstalling and updating Exploit Database..."
-sudo apt-get install exploitdb -y
+apt-get install exploitdb -y
 
 # 4. Update the Searchsploit database itself.
 echo -e "\nUpdating Searchsploit database...."
-sudo searchsploit -u
+searchsploit -u
 
 # 5. Update Nmap's Scripting Engine (NSE) scripts.
 echo -e "\nUpdating nmap script database...."
-sudo nmap --script-updatedb
+nmap --script-updatedb
 
 # --- Cleaning Section ---
 # This section helps free up disk space by removing unneeded packages and downloaded files.
 
 # 6. Remove automatically installed packages that are no longer needed by any other package.
 echo -e "\nRemoving any obsolete packages and their configuration files..."
-sudo apt-get autoremove --purge -y
+apt-get autoremove --purge -y
 
 # 7. Remove downloaded package archive files that are no longer needed.
 echo -e "\nRemoving any downloaded files that are no longer needed..."
-sudo apt-get autoclean -y
+apt-get autoclean -y
 
 # Verify the currently installed Bash version.
 echo -e "\nVerify Bash Version..."
