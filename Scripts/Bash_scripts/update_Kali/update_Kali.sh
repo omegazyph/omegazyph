5e184b13c02efdc80ae909269705a640e6852bc4
omegazyph <omegazyph@gmail.com>
2026-09-10 19:15:10 -0500

    changed the update date to today

--- Scripts/Bash_scripts/update_Kali/update_Kali.sh
+++ Scripts/Bash_scripts/update_Kali/update_Kali.sh
@@ -3,7 +3,7 @@
 # Date:         2024-01-21
 # Script Name:  update_Kali.sh
 # Author:       Wayne Stock
-# updated:      2026-09-09
+# updated:      2026-09-10
 # Description:  This script automates system maintenance tasks on Debian/Ubuntu-based systems.
 #               It performs updates, upgrades installed software, updates specific tools,
 #               and then cleans up the system.
